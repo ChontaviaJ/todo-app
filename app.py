2a0fbfad8c7c12c8cf27bc980bf1b98226e12f53
Chontavia <cjohnson734@my.gcu.edu>
2024-08-19 17:57:48 -0400

    Updated app.py with the latest changes

--- app.py
+++ app.py
@@ -141,11 +141,3 @@ def help():
 if __name__ == '__main__':
     app.run(debug=False)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# Ensure other routes are defined as needed
-
-if __name__ == '__main__':
-    app.run(debug=True)
